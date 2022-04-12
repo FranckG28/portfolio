fa38e865b711f7f89b8d15dd6bae98159b84e5dc
Franck G <fgutmann@etu.unistra.fr>
2022-04-12 23:12:40 +0200

    Added api fetch

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Projects() {
+export default function Projects({ projects, error }) {
   return (
     <>
       <Head>
@@ -10,10 +10,54 @@ export default function Projects() {
 
       <main>
         <h1>Projects</h1>
+
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          <div>
+            <p>List : </p>
+            <ul>
+              {projects.data.map((element) => {
+                return <li key={element.id}>{element.attributes.title}</li>;
+              })}
+            </ul>
+          </div>
+        )}
+
         <Link href="/">
           <a>Retour</a>
         </Link>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  let token = process.env.API_TOKEN;
+
+  const checkStatus = (resp) => {
+    if (resp.status >= 200 && resp.status < 300) {
+      return resp;
+    } else {
+      throw resp;
+    }
+  };
+
+  const parseJson = (resp) => (resp.json ? resp.json() : resp);
+
+  try {
+    const projects = await fetch("http://localhost:1338/api/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer " + token,
+      },
+    })
+      .then(checkStatus)
+      .then(parseJson);
+
+    return { props: { projects } };
+  } catch (error) {
+    return { props: { error } };
+  }
 }

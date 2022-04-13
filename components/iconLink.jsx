a2fb4d13a910e0b8edd75b0fe8ebca9da7aa00d8
Franck G <fgutmann@etu.unistra.fr>
2022-04-13 22:49:14 +0200

    Deleted old components

--- components/iconLink.jsx
+++
@@ -1,8 +0,0 @@
-export default function IconLink({children, link}) {
-
-    return (
-        <a href={link} target={'_blank'} rel="noreferrer" className="text-blue-900 hover:text-blue-400 transition-all ease-in-out duration-200 focus:text-blue-600 transform hover:scale-110 focus:scale-110">
-            <div className="w-8">{children}</div>
-        </a>);
-
-}

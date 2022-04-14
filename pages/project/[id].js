a347c22839285214d6d629b4bac3db017cbe1d72
Franck G <fgutmann@etu.unistra.fr>
2022-04-14 13:35:56 +0200

    Added project info

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -1,10 +1,32 @@
 import Layout from "../../components/layout";
-import { getProjectData, getProjectsIds } from "../../lib/projectsLib";
+import {
+  getProjectData,
+  getProjectsIds,
+  makeDate,
+} from "../../lib/projectsLib";
 
 export default function ProjectDetail({ project }) {
+  const equipe =
+    project.attributes.team == 1
+      ? "Seul"
+      : "Équipe de " + project.attributes.team;
+
+  console.log(project);
+
   return (
     <Layout title={project.attributes.title}>
-      <p>Heyy</p>
+      <div className="grid gap-3">
+        <p className="font-bold">
+          {project.attributes.tasks +
+            " • " +
+            equipe +
+            " • " +
+            makeDate(project)}
+        </p>
+
+        <p>{project.attributes.description}</p>
+        <a href={project.attributes.link}>Visiter</a>
+      </div>
     </Layout>
   );
 }
